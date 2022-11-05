133a000e2bcb4828a5a5c5240cf55059a12928d4
Collin Chan <omiyage4949@gmail.com>
2022-11-04 19:13:48 -0700

    Add more users database functions

--- scripts/profile.js
+++ scripts/profile.js
@@ -6,12 +6,46 @@
   }
 }) */
 
+let changeProfile = function(docref) {
+  let userName = document.querySelector('#username');
+  console.log(userName.value); 
+  let text = document.querySelector('#exampleFormControlTextarea1');
+  console.log(text.value); 
+
+  docref.set({
+    name: userName.value,
+    description: text.value
+  }, {merge: true}).then(() => {
+    location.reload();
+  });
+}
+
+let handleProfileChange = function(e) {
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if a user is signed in:
+    if (user) {
+        // find users document for signed in user and set fields
+        docRef = db.collection("users").doc(`${user.uid}`);
+        changeProfile(docRef);
+    } else {
+        // No user is signed in.
+    }
+  });
+}
+
+let profileButton = document.querySelector('.profile-button');
+profileButton.addEventListener('click', handleProfileChange);
+
 /* Callback that uses returned document to fill out profile page */
-let fillProfile = function (doc) {
+let fillProfile = function(doc) {
+  let placeHolders = document.querySelectorAll('.placeholder');
+  placeHolders.forEach(e => (e.style.display = 'none'))
   let profileUsername = document.querySelector('.profile .card-title');
   let profileName = document.querySelector('.profile h6');
+  let profileDescription = document.querySelector('.profile .card-text');
   profileUsername.textContent = doc.data().name;
   profileName.textContent = doc.data().email;
+  profileDescription.textContent = doc.data().description;
 };
 
 /* Initialize the profile page */
