75f9fcae088f0f79d5809132a495dbbf22d6bf04
ChinaiArman <chinaiarman@gmail.com>
2022-11-13 15:45:18 -0800

    Dynamically Populate Cards (work in progress)

--- scripts/home.js
+++ scripts/home.js
@@ -65,3 +65,26 @@ async function getCSVdata() {
     })
   })
 }
+
+
+function populateCardsDynamically() {
+  let eventCardTemplate = document.getElementById("eventCardTemplate");
+  let eventCardGroup = document.getElementById("eventCardGroup");
+  db.collection("events").get()
+    .then(allEvents => {
+      allEvents.forEach(doc => {
+        var eventName = doc.data().event; //gets the name field
+        var eventImg = doc.data().imageurl;
+        // var hikeID = doc.data().code; //gets the unique ID field
+        // var hikeLength = doc.data().length; //gets the length field
+        let testEventCard = eventCardTemplate.content.cloneNode(true);
+        testEventCard.querySelector('.card-title').innerHTML = eventName;     //equiv getElementByClassName
+        // testEventCard.querySelector('.card-length').innerHTML = hikeLength;  //equiv getElementByClassName
+        // testEventCard.querySelector('a').onclick = () => setHikeData(hikeID);//equiv getElementByTagName
+        testEventCard.querySelector('img').src = eventImg;   //equiv getElementByTagName
+        eventCardGroup.appendChild(testEventCard);
+      })
+
+    })
+}
+populateCardsDynamically();
