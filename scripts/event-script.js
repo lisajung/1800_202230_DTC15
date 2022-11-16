20d7dc1a82b2b4859fd581dede46d278f27769c7
Collin Chan <omiyage4949@gmail.com>
2022-11-15 20:33:21 -0800

    Read reviews from frestore

--- scripts/event-script.js
+++ scripts/event-script.js
@@ -21,13 +21,18 @@ let handleSaveEvent = function(e) {
 
 /* Handles the comment submit by storing a review in the reviews collection */
 let addComment = function(userDocRef) {
+    let queryStr = window.location.search;
+    let queryParams = new URLSearchParams(queryStr);
+    let eventId = queryParams.get("id");
+
     let commentTextBox = document.querySelector('.comment-text');
     let userDocId = userDocRef.id;
     docRef = db.collection("reviews").doc();
 
     docRef.set({
         text: commentTextBox.value,
-        userId: userDocId
+        userId: userDocId,
+        eventId: eventId
     }).then(() => {
         commentTextBox.value = "";
         location.reload();
@@ -90,6 +95,24 @@ let fillEventPage = function(doc) {
     eventLink.href = doc.data().link;
 };
 
+/* Fill comment section of page with appropriate firestore data */
+let fillCommentSection = function(docQuery) {
+    let commentTemplate = document.querySelector(".comment-template");
+    let commentContainer = document.querySelector(".comment-container");
+    docQuery.forEach((doc) => {
+        let userId = doc.data().userId;
+        docRef = db.collection("users").doc(`${userId}`);
+        docRef.get().then((userDoc) => {
+            let commentNode = commentTemplate.content.cloneNode(true);
+
+            commentNode.querySelector('.username').textContent = userDoc.data().name;
+            commentNode.querySelector('.text').textContent = doc.data().text;
+            commentNode.querySelector('img').src = userDoc.data().profilePictureUrl;
+            commentContainer.appendChild(commentNode);
+        });
+    });
+};
+
 /* Initialize the event page */
 let eventInit = function() {
     let queryStr = window.location.search;
@@ -98,6 +121,9 @@ let eventInit = function() {
 
     docRef = db.collection("events").doc(`${docId}`);
     docRef.get().then(fillEventPage);
+
+    collectionRef = db.collection("reviews");
+    collectionRef.where("eventId", "==", `${docId}`).get().then(fillCommentSection);
 
     /* If user is signed in then customize the page */
     firebase.auth().onAuthStateChanged((user) => {
