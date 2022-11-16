e2b40053fcc62613eb6b80da79631c3541f3bd9c
Collin Chan <omiyage4949@gmail.com>
2022-11-15 16:42:09 -0800

    Create bookmarking/save functions in event pages

--- scripts/event-script.js
+++ scripts/event-script.js
@@ -1,3 +1,48 @@
+/* Handle a save event by storing the event into current users document */
+let handleSaveEvent = function(e) {
+    let queryStr = window.location.search;
+    let queryParams = new URLSearchParams(queryStr);
+    let docId = queryParams.get("id");
+
+    /* If user is signed in then save the event page into firestore */
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            docRef = db.collection("users").doc(`${user.uid}`);
+            docRef.update({
+                savedEvents: firebase.firestore.FieldValue.arrayUnion(`${docId}`)
+            });
+
+            e.target.setAttribute('class', 'bi bi-bookmark-check');
+        } else {
+
+        }   
+    });    
+}
+
+let handleLikeEvent = function() {
+
+}
+
+/* Add interactive functionality to icons */
+let addWidgetListeners = function() {
+    let bookmarkIcon = document.querySelector('.widget-bar .bi-bookmark');
+    bookmarkIcon.addEventListener('click', handleSaveEvent);
+}
+
+/* style widgets according to current user document */
+let displayWidgetState = function(doc) {
+    let queryStr = window.location.search;
+    let queryParams = new URLSearchParams(queryStr);
+    let docId = queryParams.get("id");
+
+    let savedEventIds = doc.data().savedEvents;
+    if (savedEventIds.includes(docId)) {
+        let bookmarkIcon = document.querySelector('.widget-bar .bi-bookmark');
+        bookmarkIcon.setAttribute('class', 'bi bi-bookmark-check');
+    }
+}
+
+/* Fill event page with appropriate firestore data */
 let fillEventPage = function(doc) {
     let imgCarousel = document.querySelector('.carousel-img');
     let eventDescription = document.querySelector('.event-description');
@@ -26,5 +71,16 @@ let eventInit = function() {
 
     docRef = db.collection("events").doc(`${docId}`);
     docRef.get().then(fillEventPage);
+
+    /* If user is signed in then customize the page */
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            docRef = db.collection("users").doc(`${user.uid}`);
+            docRef.get().then(displayWidgetState);
+            addWidgetListeners();
+        } else {
+
+        }   
+    });
 };
 eventInit();
