ede0ac13a980a45badd8ef6c368d8367b102875b
ChinaiArman <chinaiarman@gmail.com>
2022-11-25 20:42:57 -0800

    change map zoom

--- scripts/event-script.js
+++ scripts/event-script.js
@@ -1,7 +1,7 @@
 let currentUser;
 
 /* Handle a remove bookmark event by removing the event from current users document and changing bookmark icon */
-function handleRemoveSaveEvent(e){
+function handleRemoveSaveEvent(e) {
     let queryStr = window.location.search;
     let queryParams = new URLSearchParams(queryStr);
     let docId = queryParams.get("id");
@@ -221,7 +221,7 @@ async function showEventsOnMap() {
 
     let doc = await db.collection("events").doc(`${eventId}`).get();
     let location = doc.data().coordinates;
-    let zoomLevel = 18;
+    let zoomLevel = 14;
 
     if (isNaN(location[0])) {
         location = [-123.11535188078236, 49.28274402264293];
@@ -244,77 +244,77 @@ async function showEventsOnMap() {
 
     map.on('load', () => {
         const features = [];
-                coordinates = doc.data().coordinates;
-                event_name = doc.data().event;
-                preview = doc.data().preview;
-                img = doc.data().posterurl;
-                //console.log([-123.11535188078236, 49.28274402264293])
-                // coordiantes = doc.data().coordiantes;
-                //console.log(coordinates);
-                url = doc.data().link;
+        coordinates = doc.data().coordinates;
+        event_name = doc.data().event;
+        preview = doc.data().preview;
+        img = doc.data().posterurl;
+        //console.log([-123.11535188078236, 49.28274402264293])
+        // coordiantes = doc.data().coordiantes;
+        //console.log(coordinates);
+        url = doc.data().link;
 
-                features.push({
-                    'type': 'Feature',
-                    'properties': {
-                        'description': `<strong>${event_name}</strong><p>${preview}</p> <img src="${img}" width="100%"> <br> <a href="/html/event.html?id=${doc.id}" target="_blank" title="Opens in a new window">Visit Here</a>`,
-                        'icon': 'mountain-15'
-                    },
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': coordinates
-                    }
-                });
+        features.push({
+            'type': 'Feature',
+            'properties': {
+                'description': `<strong>${event_name}</strong><p>${preview}</p> <img src="${img}" width="100%"> <br> <a href="/html/event.html?id=${doc.id}" target="_blank" title="Opens in a new window">Visit Here</a>`,
+                'icon': 'mountain-15'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': coordinates
+            }
+        });
 
-            map.addSource('places', {
-                // This GeoJSON contains features that include an "icon"
-                // property. The value of the "icon" property corresponds
-                // to an image in the Mapbox Streets style's sprite.
-                'type': 'geojson',
-                'data': {
-                    'type': 'FeatureCollection',
-                    'features': features
-                }
-            });
-            // Add a layer showing the places.
-            map.addLayer({
-                'id': 'places',
-                'type': 'symbol',
-                'source': 'places',
-                'layout': {
-                    'icon-image': '{icon}',
-                    'icon-allow-overlap': true
-                }
-            });
+        map.addSource('places', {
+            // This GeoJSON contains features that include an "icon"
+            // property. The value of the "icon" property corresponds
+            // to an image in the Mapbox Streets style's sprite.
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': features
+            }
+        });
+        // Add a layer showing the places.
+        map.addLayer({
+            'id': 'places',
+            'type': 'symbol',
+            'source': 'places',
+            'layout': {
+                'icon-image': '{icon}',
+                'icon-allow-overlap': true
+            }
+        });
 
-            // When a click event occurs on a feature in the places layer, open a popup at the
-            // location of the feature, with description HTML from its properties.
-            map.on('click', 'places', (e) => {
-                // Copy coordinates array.
-                const coordinates = e.features[0].geometry.coordinates.slice();
-                const description = e.features[0].properties.description;
+        // When a click event occurs on a feature in the places layer, open a popup at the
+        // location of the feature, with description HTML from its properties.
+        map.on('click', 'places', (e) => {
+            // Copy coordinates array.
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const description = e.features[0].properties.description;
 
-                // Ensure that if the map is zoomed out such that multiple
-                // copies of the feature are visible, the popup appears
-                // over the copy being pointed to.
-                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                }
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
 
-                new mapboxgl.Popup()
-                    .setLngLat(coordinates)
-                    .setHTML(description)
-                    .addTo(map);
-            });
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        });
 
-            // Change the cursor to a pointer when the mouse is over the places layer.
-            map.on('mouseenter', 'places', () => {
-                map.getCanvas().style.cursor = 'pointer';
-            });
+        // Change the cursor to a pointer when the mouse is over the places layer.
+        map.on('mouseenter', 'places', () => {
+            map.getCanvas().style.cursor = 'pointer';
+        });
 
-            // Change it back to a pointer when it leaves.
-            map.on('mouseleave', 'places', () => {
-                map.getCanvas().style.cursor = '';
-            });
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', 'places', () => {
+            map.getCanvas().style.cursor = '';
+        });
 
     });
 }
